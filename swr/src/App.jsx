69348e39d71ee97d3c704dd5765edea8825b02cd
HOSENUR <nur08439@gmail.com>
2022-10-29 18:30:36 +0530

    SWR Fetch

--- swr/src/App.jsx
+++ swr/src/App.jsx
@@ -1,34 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import useSWR from 'swr'
+const fetcher = (...args) => console.log(args)
 function App() {
-  const [count, setCount] = useState(0)
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const { data, error } = useSWR("https://random-data-api.com/api/v2/users", fetcher)
+  console.log(data)
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  // render data
+  return <div>hello {data.first_name}!</div>
 }
 
 export default App
